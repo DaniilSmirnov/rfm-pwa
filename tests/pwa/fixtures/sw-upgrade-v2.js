893db6b9cde9e0f65041df6047b1adfda4ad0cc0
Daniil <8039921+DaniilSmirnov@users.noreply.github.com>
2026-09-24 17:04:18 +0200

    test: add service worker upgrade harness

---
+++ tests/pwa/fixtures/sw-upgrade-v2.js
@@ -0,0 +1,9 @@
+const VERSION='v2';
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{
+  const url=new URL(event.request.url);
+  if(url.pathname==='/__sw-version'){
+    event.respondWith(new Response(VERSION,{headers:{'content-type':'text/plain'}}));
+  }
+});
