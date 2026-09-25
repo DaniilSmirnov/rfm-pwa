b94f34861b1d8d364a4e801d2abb5ecfe77cd6f5
Daniil <8039921+DaniilSmirnov@users.noreply.github.com>
2026-09-25 09:31:55 +0200

    Use shared network timeouts for push requests

--- src/app/push-client.js
+++ src/app/push-client.js
@@ -3,6 +3,7 @@ import { buildRaceReminders } from './schedule.js';
 import { subscribedStageKeys } from './preferences.js';
 import { isIOSDevice, isStandalonePwa, syncInstallUi, requestPwaInstall } from './pwa.js';
 import { fetchWithTimeout } from './net.js';
+import { fetchWithTimeout } from './net.js';
 
 const $=id=>document.getElementById(id);
 
